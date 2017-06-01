e445874e2fb9e2efd4e238c77f7ca3290287e4d7
Sven <Gorementor@users.noreply.github.com>
2017-06-01 19:57:44 +0200

    Add project files.

---
+++ GPS_Logger/GPS_Logger.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GPS_Logger.ino
+ Created:	6/1/2017 7:57:36 PM
+ Author:	Sven
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
